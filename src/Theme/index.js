5dc0ddcdbb4ccc7023fe0447af02883e042bd794
Jim X. Lin <miljinx@gmail.com>
2020-12-10 22:02:23 -0800

    Update colors and background to match YFLINK theme

--- src/Theme/index.js
+++ src/Theme/index.js
@@ -24,7 +24,7 @@ const theme = (darkMode, color) => ({
   shadowColor: darkMode ? '#000' : '#2F80ED',
   mercuryGray: darkMode ? '#333333' : '#E1E1E1',
 
-  text1: darkMode ? '#FAFAFA' : '#1F1F1F',
+  text1: darkMode ? '#FFFFFF' : '#000000',
   text2: darkMode ? '#C3C5CB' : '#565A69',
   text3: darkMode ? '#6C7284' : '#888D9B',
   text4: darkMode ? '#565A69' : '#C3C5CB',
@@ -34,28 +34,28 @@ const theme = (darkMode, color) => ({
   white: '#FFFFFF',
 
   // backgrounds / greys
-  bg1: darkMode ? '#212429' : '#FAFAFA',
+  bg1: darkMode ? '#222A35' : '#FFFFFF',
   bg2: darkMode ? '#2C2F36' : '#F7F8FA',
   bg3: darkMode ? '#40444F' : '#EDEEF2',
-  bg4: darkMode ? '#565A69' : '#CED0D9',
-  bg5: darkMode ? '#565A69' : '#888D9B',
-  bg6: darkMode ? '#000' : '#FFFFFF',
+  bg4: darkMode ? '#5F656D' : '#CED0D9',
+  bg5: darkMode ? '#6C7284' : '#888D9B',
+  bg6: darkMode ? '#373F49' : '#FFFFFF',
 
   //specialty colors
-  modalBG: darkMode ? 'rgba(0,0,0,0.85)' : 'rgba(0,0,0,0.6)',
-  advancedBG: darkMode ? 'rgba(0,0,0,0.1)' : 'rgba(255,255,255,0.4)',
-  onlyLight: darkMode ? '#22242a' : 'transparent',
+  modalBG: darkMode ? 'rgba(0,0,0,.425)' : 'rgba(0,0,0,0.3)',
+  advancedBG: darkMode ? 'rgb(34, 42, 53)' : 'rgba(255,255,255,0.6)',
+  onlyLight: darkMode ? 'transparent' : 'transparent',
   divider: darkMode ? 'rgba(43, 43, 43, 0.435)' : 'rgba(43, 43, 43, 0.035)',
 
   //primary colors
-  primary1: darkMode ? '#2172E5' : '#ff007a',
+  primary1: darkMode ? '#295BDB' : '#ff007a',
   primary2: darkMode ? '#3680E7' : '#FF8CC3',
   primary3: darkMode ? '#4D8FEA' : '#FF99C9',
   primary4: darkMode ? '#376bad70' : '#F6DDE8',
-  primary5: darkMode ? '#153d6f70' : '#FDEAF1',
+  primary5: darkMode ? '#202F46' : '#FDEAF1',
 
   // color text
-  primaryText1: darkMode ? '#6da8ff' : '#ff007a',
+  primaryText1: darkMode ? '#87A9FF' : '#ff007a',
 
   // secondary colors
   secondary1: darkMode ? '#2172E5' : '#ff007a',
@@ -143,9 +143,6 @@ export const ThemedBackground = styled.div`
   pointer-events: none;
   max-width: 100vw !important;
   height: 200vh;
-  mix-blend-mode: color;
-  background: ${({ backgroundColor }) =>
-    `radial-gradient(50% 50% at 50% 50%, ${backgroundColor} 0%, rgba(255, 255, 255, 0) 100%)`};
   position: absolute;
   top: 0px;
   left: 0px;
@@ -154,6 +151,7 @@ export const ThemedBackground = styled.div`
   transform: translateY(-110vh);
 `
 
+// TODO: create a light background-image for light mode
 export const GlobalStyle = createGlobalStyle`
   @import url('https://rsms.me/inter/inter.css');
   html { font-family: 'Inter', sans-serif; }
@@ -168,7 +166,12 @@ export const GlobalStyle = createGlobalStyle`
     width: 100%;
     height: 100%;
     font-size: 14px;    
-    background-color: ${({ theme }) => theme.bg6};
+    min-height: 100vh;
+    background-image: url("https://yflink.io/YFL-BG-pattern-left.svg");
+    background-position: 0 10vh;
+    background-attachment: fixed;
+    background-repeat: no-repeat;
+    background-color: #2B3A4A;
   }
 
   a {
